891fc87eb07f81d6040c7620daeb091f8462b0f8
Maks0u <Maks0u@proton.me>
2025-05-23 18:00:58 +0200

    feat(zsh): update custom git log

--- .oh-my-zsh/custom/git.zsh
+++ .oh-my-zsh/custom/git.zsh
@@ -1,32 +1,36 @@
 # git log graph with custom columns
 # $1: width (default terminal width)
-# $2: commit limit (default 500)
 git_log_columns() {
     local width=$((${1:-$(tput cols)} - 1))
+    [[ ! -z $1 ]] && shift
+
     local hashWidth=14
-    local authorWidth=9
-    local messageWidth=$((width - hashWidth - authorWidth - 17))
+    local authorWidth=12
+    local dateWidth=12
 
     local config="%w(${width})%C(auto)"
-    local hash="%<|(${hashWidth})%C(blue)%h %C(auto)"
-    local message="%<(${messageWidth},trunc)%s "
-    local author="%C(blue)%<(${authorWidth},trunc)%an%C(auto) "
-    local date="%C(green)%>|(${width})%ar%C(auto)"
+    local hash="%>|(${hashWidth})%C(blue)%h%C(auto)"
     local decorate="%D"
+    local message="%<|($((width - authorWidth - dateWidth - 2)),trunc)%s"
+    local author="%C(blue)%<(${authorWidth},trunc)%an%C(auto)"
+    local date="%C(green)%>|($width,trunc)%ar%C(auto)"
 
-    git log --all --graph --max-count=${2:-500} \
-        --pretty=format:"${config}${hash}${message}${author}${date} ${decorate}"
+    git log --all --graph --color \
+        --pretty=format:"${config}${hash} ${decorate}  ${message} ${author} ${date}" \
+        "${@}"
 }
 
 # alias for git_log_columns
 gloc() {
-    git_log_columns "${1:-80}" "${@}"
+    git_log_columns "${@}"
 }
 
 # watch alias for git_log_columns
 glocw() {
-    watch --color --interval 1 --no-title -x zsh -c '. $ZSH/custom/git.zsh; gloc $(tput cols) 50'
+    watch --color --interval 3 --no-title -x zsh -c '. $ZSH/custom/git.zsh; gloc $(tput cols) -60'
 }
 
-alias gsbw='watch -ctn 1 git status --short --branch'
-alias gssw='watch -ctn 1 git status --short'
+# git status short watch
+alias gssw='watch -ctn 3 git status --short'
+# git status branch watch
+alias gsbw='watch -ctn 3 git status --short --branch'
