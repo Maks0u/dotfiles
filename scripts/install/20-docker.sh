85e21546c493ac2cded1c9296b9783aa5453994c
Maks0u <Maks0u@proton.me>
2026-05-10 21:46:20 +0200

    feat(docker): install

---
+++ scripts/install/20-docker.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+if [[ ! "${EUID}" -eq 0 ]]; then
+    echo "Please run as root using 'sudo -E $0'"
+    exit 1
+fi
+
+if ! command -v dnf >/dev/null; then
+    printf '\n%s\n\n' 'OS not supported, please use Fedora based OS with DNF installed.'
+    exit 1
+fi
+
+# https://docs.docker.com/engine/install/rhel/
+# https://docs.docker.com/engine/install/fedora/
+
+dnf remove docker \
+    docker-client \
+    docker-client-latest \
+    docker-common \
+    docker-latest \
+    docker-latest-logrotate \
+    docker-logrotate \
+    docker-selinux \
+    docker-engine-selinux \
+    docker-engine \
+    podman \
+    runc
+
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+systemctl enable docker.service
+systemctl enable containerd.service
+
+printf '\n%s\n\n' 'Installation done, run `sudo usermod -aG docker <user>` then start a new session to be able to run docker as user.'
