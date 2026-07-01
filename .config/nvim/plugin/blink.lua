a2290a9809fe053d3430be6bd2b676ead14dd1ec
Maks0u <Maks0u@proton.me>
2026-07-01 21:05:14 +0200

    feat(nvim): snippets

--- .config/nvim/plugin/blink.lua
+++ .config/nvim/plugin/blink.lua
@@ -1,6 +1,7 @@
 vim.pack.add({
     'https://github.com/saghen/blink.lib',
     'https://github.com/saghen/blink.cmp',
+    'https://github.com/rafamadriz/friendly-snippets',
 })
 
 local cmp = require('blink.cmp')
