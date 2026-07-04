b629ca656aecd006eee867c883e3525183963293
Maks0u <Maks0u@proton.me>
2026-07-04 22:48:54 +0200

    feat(nvim): gitsigns functions

--- .config/nvim/plugin/git.lua
+++ .config/nvim/plugin/git.lua
@@ -13,17 +13,42 @@ gitsigns.setup({
     },
 })
 
-vim.keymap.set('n', '<Leader>gb', function()
-    gitsigns.blame()
-end, { desc = 'Git [b]lame' })
+local find_win = function (filetype)
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+        local buf = vim.api.nvim_win_get_buf(win)
+        if vim.bo[buf].filetype == filetype then
+            return win
+        end
+    end
+end
+
+local toggle_blame = function ()
+    local blame_win = find_win('gitsigns-blame')
+    if blame_win then
+        vim.api.nvim_win_close(blame_win, false)
+    else
+        gitsigns.blame()
+    end
+end
+
+vim.keymap.set('n', '<Leader>gb', toggle_blame, { desc = 'Git [b]lame' })
 
 vim.keymap.set('n', '<Leader>gl', function()
     gitsigns.blame_line({ full = true })
 end, { desc = 'Git blame [l]ine' })
 
-vim.keymap.set('n', '<Leader>gd', function()
-    gitsigns.diffthis()
-end, { desc = 'Git [d]iff' })
+local toggle_diff = function ()
+    if vim.wo.diff then
+        vim.cmd([[
+            wincmd h
+            quit
+        ]])
+    else
+        gitsigns.diffthis()
+    end
+end
+
+vim.keymap.set('n', '<Leader>gd', toggle_diff, { desc = 'Git [d]iff' })
 
 vim.keymap.set('n', ']g', function()
     gitsigns.nav_hunk('next')
@@ -33,15 +58,13 @@ vim.keymap.set('n', '[g', function()
     gitsigns.nav_hunk('prev')
 end, { desc = '[G]it previous hunk' })
 
-vim.keymap.set('n', '<Leader>gp', function()
-    gitsigns.preview_hunk()
-end, { desc = 'Git [p]review hunk' })
-
-vim.keymap.set('n', '<Leader>gi', function()
-    gitsigns.preview_hunk_inline()
-end, { desc = 'Git preview hunk [i]nline' })
+vim.keymap.set('n', '<Leader>gp', gitsigns.preview_hunk, { desc = 'Git [p]review hunk' })
+vim.keymap.set('n', '<Leader>gi', gitsigns.preview_hunk_inline, { desc = 'Git preview hunk [i]nline' })
 
 vim.keymap.set('n', '<Leader>gs', '<cmd>Gitsigns stage_hunk<CR>', { desc = 'Git [s]tage or unstage hunk' })
 vim.keymap.set('x', '<Leader>gs', "<cmd>'<,'>Gitsigns stage_hunk<CR>", { desc = 'Git [s]tage or unstage hunk' })
 vim.keymap.set('n', '<Leader>gr', '<cmd>Gitsigns reset_hunk<CR>', { desc = 'Git [r]eset hunk' })
 vim.keymap.set('x', '<Leader>gr', "<cmd>'<,'>Gitsigns reset_hunk<CR>", { desc = 'Git [r]eset hunk' })
+
+vim.keymap.set('n', '<Leader>gS', '<cmd>Gitsigns stage_buffer<CR>', { desc = 'Git [S]tage or unstage buffer' })
+vim.keymap.set('n', '<Leader>gR', '<cmd>Gitsigns reset_buffer<CR>', { desc = 'Git [R]eset buffer' })
