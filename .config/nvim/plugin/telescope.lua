f323d538508e90016df56b93e0b0077d8e339fbd
Maks0u <Maks0u@proton.me>
2026-07-04 21:00:51 +0200

    feat(nvim): telescope search in hidden files

--- .config/nvim/plugin/telescope.lua
+++ .config/nvim/plugin/telescope.lua
@@ -5,7 +5,21 @@ vim.pack.add({
     'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
 })
 
-require('telescope').setup({})
+local vimgrep_arguments = { unpack(require('telescope.config').values.vimgrep_arguments) }
+table.insert(vimgrep_arguments, '--hidden')
+table.insert(vimgrep_arguments, '--glob')
+table.insert(vimgrep_arguments, '!**/.git/*')
+
+require('telescope').setup({
+    defaults = {
+        vimgrep_arguments = vimgrep_arguments,
+    },
+    pickers = {
+        find_files = {
+            find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
+        },
+    },
+})
 
 pcall(require('telescope').load_extension, 'fzf')
 pcall(require('telescope').load_extension, 'ui-select')
