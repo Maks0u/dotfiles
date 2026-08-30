1d0689232c1c93c4f99952d48a30529a47ec5348
Maks0u <Maks0u@proton.me>
2026-08-30 18:05:55 +0200

    feat(nvim): telescope layout

--- .config/nvim/plugin/telescope.lua
+++ .config/nvim/plugin/telescope.lua
@@ -12,6 +12,8 @@ table.insert(vimgrep_arguments, '!**/.git/*')
 
 require('telescope').setup({
     defaults = {
+        layout_config = { anchor = 'S', height = 0.3, width = 140 },
+        layout_strategy = 'center',
         vimgrep_arguments = vimgrep_arguments,
     },
     pickers = {
@@ -26,10 +28,15 @@ pcall(require('telescope').load_extension, 'ui-select')
 
 local builtin = require('telescope.builtin')
 
+local nvim_config_files = function()
+    builtin.find_files({ cwd = vim.fn.stdpath('config') })
+end
+
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[D]iagnostics' })
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[F]iles' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[G]rep' })
 vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[K]eymaps' })
+vim.keymap.set('n', '<leader>sn', nvim_config_files, { desc = '[N]vim config files' })
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[R]esume' })
 vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[.] Recent Files' })
 vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Buffers' })
